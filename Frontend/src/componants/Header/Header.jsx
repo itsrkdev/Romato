4c84d226080d5245cec248323001bc5dfae43240
Rabindra kushwaha <rabindrakushwaha0001@gmail.com>
2026-05-23 18:05:32 +0530

    Update Header.jsx

--- Frontend/src/componants/Header/Header.jsx
+++ Frontend/src/componants/Header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css'
 export default function Header() {
 
   return (
-    <div className='header'>
+    <div className='header' id="home">
         <div className="header-contents">
             <h2>Order your favourite food here</h2>
             <p>Choose from a diverse menu feature a dlectable array of dishes crafted with the finest ingredients and culinary exepertise. Our mission is to satisfy your cravings and elevate your dainig exeperince, one delicious meal at time.</p>
