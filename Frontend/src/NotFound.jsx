6d5e45d081af096b70fcc077eaba5d223d171b81
Rabindra kushwaha <rabindrakushwaha0001@gmail.com>
2026-05-23 18:24:59 +0530

    Create NotFound.jsx

---
+++ Frontend/src/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function NotFound() {
+
+    
+  return (
+    <div style={{ textAlign: 'center', padding: '100px 20px' }}>
+      <h2>🍅 Romato - 404 Page Not Found</h2>
+      <p>Oops! Jo page aap dhoondh rahe hain wo maujood nahi hai.</p>
+      <a href="/" style={{ color: '#ff4321', fontWeight: 'bold' }}>Go Back to Home</a>
+    </div>
+
+  )
+}
