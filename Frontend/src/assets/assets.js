e8a0a5273ed0240cacf2beaaf6214e9da3c2a94f
Rabindra kushwaha <rabindrakushwaha0001@gmail.com>
2026-05-25 10:13:54 +0530

    Include scanner image in asset imports
    Added missing scanner image import to assets.

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -2,7 +2,7 @@ import basket_icon from './basket_icon.png'
 import logo from './logo.png'
 import header_img from './header_img.png'
 import search_icon from './search_icon.png'
-
+import scanner from './scanner.jpeg'
 
 import add_icon_white from './add_icon_white.png'
 import add_icon_green from './add_icon_green.png'
